c7c9c31b70834000bbfd77b4822e934cd93602df
Lukas0210 <lukasparacampos@gmail.com>
2026-05-05 18:56:09 -0300

    add de ENUM

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ const machineScore = document.querySelector('#computer-score')
 let humanPoints = 0
 let machinePoints = 0
 
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
 const playHuman = (humanChoice) => {
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
 
     console.log(randomNumber);
@@ -23,9 +29,9 @@ const playTheGame = (human, machine) => {
     if (human === machine) {
         result.innerHTML = 'Empate'
     } else if (
-        (human === 'rock' && machine === 'scissors') ||
-        (human === 'paper' && machine === 'rock') ||
-        (human === 'scissors' && machine === 'paper')
+        (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) ||
+        (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) ||
+        (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)
     ) {
         humanPoints++
         humanScore.innerHTML = humanPoints
